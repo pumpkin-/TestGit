361b411b45296c3d64dd3bd9f6fe2de461ec981d
hmy <614476446@qq.com>
2015-10-07 22:11:07 +0800

    ，，

--- TestGit/app/src/main/java/pb/dada/com/testgit/MainActivity.java
+++ TestGit/app/src/main/java/pb/dada/com/testgit/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
-    int i=0;
+    int i=10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
